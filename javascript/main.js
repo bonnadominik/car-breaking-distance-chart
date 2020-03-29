3aab2a519e6f337dae6d498c299b2dbbcfbf431d
Dominik Bonna <bonna.dominik@gmail.com>
2020-03-29 16:47:43 +0200

    ended simple styles

--- javascript/main.js
+++ javascript/main.js
@@ -19,15 +19,16 @@ let CalculateBreakingDistanceOnWet = (arrayOfVelocitys) => {
 
 let chart = document.getElementById('breaking-distance-chart').getContext('2d');
 
-// Chart.defaults.global.defaultFontFamily = 'Lato';
+Chart.defaults.global.defaultFontFamily = 'Lato';
 Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#fff';
 
 let breakingDistanceChart = new Chart(chart, {
     type: 'line',
     data: {
         labels: velocitys,
         datasets:[{
-            label: 'Droga hamowania samochodu na suchej nawierzchni',
+            label: 'Droga hamowania samochodu na suchej nawierzchni w metrach',
             data: CalculateBreakingDistanceOnDry(velocitys),
             borderWidth: 2,
             borderColor: '#1a1b4b',
@@ -36,7 +37,7 @@ let breakingDistanceChart = new Chart(chart, {
             hoverBorderWidth: 3,
             fill: 'none'
         },{
-            label: 'Droga hamowania samochodu na mokrej nawierzchni',
+            label: 'Droga hamowania samochodu na mokrej nawierzchni w metrach',
             data: CalculateBreakingDistanceOnWet(velocitys),
             borderWidth: 2,
             borderColor: '#f7a400',
@@ -47,22 +48,36 @@ let breakingDistanceChart = new Chart(chart, {
         }]
     },
     options: {
-        title:{
-            display: true,
-            text: 'Wykres drogi hamowania samochodu na suchej i mokrej nawierzchni',
-        },
         legend:{
             position: 'bottom',
             labels: {
-                fontSize: 16
+                fontSize: 13
             }
         },
-        layout: {
-            padding: {
-                left: 20,
-                right: 20
+        layout:{
+            padding:{
+                top: 50,
+                left: 50,
+                right: 50,
+                bottom: 50
             }
         },
+        responsive: true,
+        maintainAspectRatio: false,
+        scales:{
+            yAxes :[{
+                scaleLabel:{
+                    display: true,
+                    labelString: "odległość w m"
+                }
+            }],
+            xAxes :[{
+                scaleLabel:{
+                    display: true,
+                    labelString: "prędkość w km/h"
+                }
+            }]
+        }
     }
 });
 
